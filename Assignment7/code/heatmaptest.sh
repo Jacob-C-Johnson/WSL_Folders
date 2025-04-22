365bb2468136416811ace89951b9e52f0a6528ea
Jacob-C-Johnson <jacobj1789@gmail.com>
2025-04-22 15:24:18 -0400

    added heatmaptest (Assignment 7)

---
+++ Assignment7/code/heatmaptest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=Heatmap
+#SBATCH --output=heatmap_%j.out
+#SBATCH --partition=compute
+#SBATCH --time=00:10:00
+#SBATCH --mem=4G
+
+module purge
+module load python/3.8-anaconda   # or your site’s python+matplotlib
+
+python3 heatmap.py
