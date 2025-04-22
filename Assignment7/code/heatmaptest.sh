74667bb78de2d67a9f438c29db1e6f7058f75c1e
Jacob-C-Johnson <jacobj1789@gmail.com>
2025-04-22 15:25:32 -0400

    added acc to test script assignment 7

--- Assignment7/code/heatmaptest.sh
+++ Assignment7/code/heatmaptest.sh
@@ -4,6 +4,7 @@
 #SBATCH --partition=compute
 #SBATCH --time=00:10:00
 #SBATCH --mem=4G
+#SBATCH --account=ccu108
 
 module purge
 module load python/3.8-anaconda   # or your site’s python+matplotlib
