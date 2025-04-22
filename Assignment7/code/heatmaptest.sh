169665212be63dc7cf47166807f06ed65da789f0
Jacob-C-Johnson <jacobj1789@gmail.com>
2025-04-22 15:30:29 -0400

    yacfa7

--- Assignment7/code/heatmaptest.sh
+++ Assignment7/code/heatmaptest.sh
@@ -5,6 +5,8 @@
 #SBATCH --time=00:10:00
 #SBATCH --mem=4G
 #SBATCH --account=ccu108
+#SBATCH --ntasks=1               # <— add this
+#SBATCH --ntasks-per-node=1      # optional, but explicit
 
 module purge
 module load python/3.8-anaconda   # or your site’s python+matplotlib
